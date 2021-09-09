a21006b66ef83d7c76ac6c3a9ceab109f2734019
Vinicius Jimenez <vinnicius.jimenez@gmail.com>
2021-09-09 18:52:47 -0300

    Play/Pause button added

--- AudioPlayer.js
+++ AudioPlayer.js
@@ -1,4 +1,12 @@
+const createButton = ({ icon, label }) => `
+    <img src=${icon} />
+    <span>${label}</span>
+`
+
 class AudioPlayer{
+    static PLAY_ICON = './assets/play.svg'
+    static PAUSE_ICON ='./assets/pause.svg'
+
     constructor({ audio, trigger }){
         this.audio = audio
         this.trigger = trigger
@@ -10,18 +18,33 @@ class AudioPlayer{
             status || this.pause()
         })
 
-        this.audio.addEventListener('ended', () => this.trigger.textContent = 'ESCUTAR')
-    }
 
+        this.audio.addEventListener('ended', () => {
+            this.trigger.innerHTML = createButton({ 
+                icon: AudioPlayer.PLAY_ICON,
+                label: 'Escutar'
+            })
+        })
+    }
+    
     play(){
         this.audio.play()
-        this.trigger.textContent = 'PAUSAR'
+
+        this.trigger.innerHTML = createButton({
+            icon: AudioPlayer.PAUSE_ICON,
+            label: 'Pausar'
+        })
     }
 
     pause(){
         this.audio.pause()
-        this.trigger.textContent = 'CONTINUAR'
+        
+        this.trigger.innerHTML = createButton({
+            icon: AudioPlayer.PLAY_ICON,
+            label: 'Continuar'
+        })
     }
+
 
     static initialize({ audio, trigger }){
         return new AudioPlayer({ audio, trigger })
